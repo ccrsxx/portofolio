91c14a711f97924e8b638dc26e646d531c595c65
ccrsxx <aminrisal@gmail.com>
2022-12-10 21:03:14 +0700

    style: add bunch of custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const { blue } = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         inter: ['var(--font-inter)']
       },
-      colors: {}
+      colors: {
+        background: '#222222',
+        'accent-blue': blue[400]
+      }
     }
   },
   plugins: []
