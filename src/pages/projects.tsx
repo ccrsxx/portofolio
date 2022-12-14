d6645db3f14e39a8c5573e321396329351729c47
ccrsxx <aminrisal@gmail.com>
2022-12-14 21:37:19 +0700

    feat: add placeholder pages

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,3 +1,22 @@
+import { motion } from 'framer-motion';
+import type { Variants } from 'framer-motion';
+
+const item: Variants = {
+  initial: { opacity: 0, y: 50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 }
+};
+
 export default function Placeholder(): JSX.Element {
-  return <main className='layout'>Placeholder</main>;
+  return (
+    <motion.main
+      className='layout min-h-screen pt-32 text-center'
+      variants={item}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+    >
+      Placeholder
+    </motion.main>
+  );
 }
