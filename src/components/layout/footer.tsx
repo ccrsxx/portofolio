79618afbc6bbc3324105be11ae80b99866700356
ccrsxx <aminrisal@gmail.com>
2022-12-14 21:38:35 +0700

    feat: finish main layout

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,142 @@
+import Link from 'next/link';
+import { Tooltip } from '@components/ui/tooltip';
+import { ReactIcon } from '@components/ui/react-icon';
+import type { IconName } from '@components/ui/react-icon';
+
+type FooterLink = {
+  name: string;
+  href: string;
+  tip: string | JSX.Element;
+  out?: boolean;
+};
+
 export function Footer(): JSX.Element {
-  return <footer className='border-gray-600'></footer>;
+  return (
+    <footer className='main-border layout grid gap-4 border-0 border-t pb-2'>
+      <nav className='mt-6 flex justify-center gap-8'>
+        {footerLinks.map(({ name, href, tip, out }) => (
+          <Tooltip tip={tip} key={name}>
+            <Link
+              className='text-sm font-medium dark:text-gray-200'
+              href={href}
+              {...(out && { target: '_blank', rel: 'noreferrer' })}
+            >
+              {name}
+            </Link>
+          </Tooltip>
+        ))}
+      </nav>
+      <section className='mt-4 grid justify-items-center gap-2 text-gray-600 dark:text-gray-300'>
+        <h2 className='font-medium'>Reach me out</h2>
+        <section className='flex gap-4'>
+          {socialLinks.map(({ name, href, icon, tip }) => (
+            <Tooltip tip={tip} key={name}>
+              <a
+                className='transition-colors hover:text-accent-blue'
+                href={href}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <ReactIcon iconName={icon} />
+              </a>
+            </Tooltip>
+          ))}
+        </section>
+      </section>
+      <p className='mt-4 text-center text-sm text-gray-600 dark:text-gray-300'>
+        &copy; Risal Amin 2022 •{' '}
+        <a
+          className='transition hover:text-gray-800 dark:hover:text-gray-100'
+          href='https://github.com/ccrsxx/ccrsxx.me'
+        >
+          Got any feedback?
+        </a>
+      </p>
+    </footer>
+  );
 }
+
+const footerLinks: FooterLink[] = [
+  {
+    name: 'Source code',
+    href: 'https://github.com/ccrsxx/ccrsxx.me',
+    tip: (
+      <p>
+        This website is <strong>open source!</strong>
+      </p>
+    ),
+    out: true
+  },
+  {
+    name: 'Design',
+    href: '/design',
+    tip: 'ccrsxx.me color palette'
+  },
+  {
+    name: 'Analytics',
+    href: 'https://umami.ccrsxx.me',
+    tip: 'ccrsxx.me views and visitors analytics',
+    out: true
+  },
+  {
+    name: 'Statistics',
+    href: '/statistics',
+    tip: 'Blog & Projects statistics'
+  },
+  {
+    name: 'Subscribe',
+    href: '/subscribe',
+    tip: 'Get notified when I publish a new post'
+  }
+];
+
+type SocialLink = {
+  name: string;
+  href: string;
+  icon: IconName;
+  tip: JSX.Element;
+  tooltipClassName?: string;
+};
+
+const socialLinks: SocialLink[] = [
+  {
+    name: 'Email',
+    href: 'mailto:me@ccrsxx.me',
+    icon: 'HiEnvelope',
+    tip: (
+      <p className='text-center'>
+        Contact me at <span className='gradient-title'>me@ccrsxx.me</span>
+      </p>
+    )
+  },
+  {
+    name: 'GitHub',
+    href: 'https://github.com/ccrsxx',
+    icon: 'SiGithub',
+    tip: (
+      <p>
+        See my other projects at <span className='gradient-title'>GitHub</span>
+      </p>
+    )
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://linkedin.com/in/risalamin',
+    icon: 'SiLinkedin',
+    tip: (
+      <p>
+        Find me on <span className='gradient-title'>LinkedIn</span>
+      </p>
+    )
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/ccrsxx',
+    icon: 'SiTwitter',
+    tip: (
+      <p>
+        Follow me on <span className='gradient-title'>Twitter</span>
+      </p>
+    )
+  }
+];
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,48 @@
 import Link from 'next/link';
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
 import { clsx } from 'clsx';
 import { useRouter } from 'next/router';
 import { ThemeSwitch } from '@components/common/theme-switch';
+
+export function Header(): JSX.Element {
+  const ref = useRef<HTMLDivElement>(null);
+  const inView = useInView(ref, { margin: '40px', amount: 'all' });
+
+  const { pathname } = useRouter();
+
+  return (
+    <>
+      <div ref={ref} />
+      <header
+        className={clsx(
+          !inView && 'shadow-sm',
+          'fixed z-10 w-full bg-white/60 backdrop-blur-md transition dark:bg-background/60'
+        )}
+      >
+        <div className='h-2 bg-gradient-to-r from-blue-400 to-green-300' />
+        <div className='layout flex items-center justify-between py-4'>
+          <nav className='flex gap-4 font-medium'>
+            {navLinks.map(({ name, href }, index) => (
+              <Link
+                className={clsx(
+                  'smooth-tab transition-colors hover:text-blue-200 hover:delay-[0ms]',
+                  pathname === href && 'gradient-title',
+                  `delay-${index * 100}`
+                )}
+                href={href}
+                key={name}
+              >
+                {name}
+              </Link>
+            ))}
+          </nav>
+          <ThemeSwitch />
+        </div>
+      </header>
+    </>
+  );
+}
 
 const navLinks = [
   { name: 'Home', href: '/' },
@@ -9,30 +50,3 @@ const navLinks = [
   { name: 'Blog', href: '/blog' },
   { name: 'About', href: '/about' }
 ];
-
-export function Header(): JSX.Element {
-  const { pathname } = useRouter();
-
-  return (
-    <header className='sticky top-0 bg-white/60 backdrop-blur-md transition-colors dark:bg-background/60'>
-      <div className='h-2 bg-gradient-to-r from-blue-400 via-blue-300 to-blue-200' />
-      <div className='layout flex items-center justify-between py-4'>
-        <nav className='flex gap-4'>
-          {navLinks.map(({ name, href }) => (
-            <Link
-              className={clsx(
-                pathname === href && '!text-accent-blue',
-                'smooth-tab transition-colors hover:text-blue-200'
-              )}
-              href={href}
-              key={name}
-            >
-              {name}
-            </Link>
-          ))}
-        </nav>
-        <ThemeSwitch />
-      </div>
-    </header>
-  );
-}
