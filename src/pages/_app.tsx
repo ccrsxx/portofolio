77ad45534c109e12b60247c7ca2fcdd89d3b1af9
ccrsxx <aminrisal@gmail.com>
2022-12-14 21:40:33 +0700

    feat: add exit animation when navigating between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,9 @@
 import '@styles/globals.scss';
 
 import nProgress from 'nprogress';
-import { Router } from 'next/router';
+import { AnimatePresence } from 'framer-motion';
+import { Router, useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { Layout } from '@components/layout/layout';
 import { AppHead } from '@components/common/app-head';
@@ -16,12 +18,21 @@ Router.events.on('routeChangeError', nProgress.done);
 Router.events.on('routeChangeComplete', nProgress.done);
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    const popAudio = new Audio('/assets/pop.mp3');
+    void popAudio.play();
+  }, [pathname]);
+
   return (
     <>
       <AppHead />
       <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
         <Layout>
-          <Component {...pageProps} />
+          <AnimatePresence mode='wait'>
+            <Component {...pageProps} key={pathname} />
+          </AnimatePresence>
         </Layout>
       </ThemeProvider>
     </>
