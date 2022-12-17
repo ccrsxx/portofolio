1c7b13a2f2a3ff066788f77a1405b4960df13a5e
ccrsxx <aminrisal@gmail.com>
2022-12-17 07:26:46 +0000

    feat: import blog image

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,3 +1,5 @@
 import TwitterClone from '/public/assets/projects/twitter-clone.png';
+import FirstPost from '/public/assets/blogs/first-post.jpg';
+import SecondPost from '/public/assets/blogs/second-post.jpg';
 
-export { TwitterClone };
+export { TwitterClone, FirstPost, SecondPost };
