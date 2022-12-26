a06034c5df672e6b1449aeb1829c87a3eb982fca
ccrsxx <aminrisal@gmail.com>
2022-12-26 22:07:44 +0700

    feat: implement index and blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,15 @@
+import { readdir } from 'fs/promises';
+import { join } from 'path/posix';
 import { motion } from 'framer-motion';
+import { getContentReadTime, getMetaFromDb, getTags } from '@lib/mdx-utils';
+import { SEO } from '@components/common/seo';
+import { Tag } from '@components/blog/tag';
+import { SortListbox } from '@components/blog/sort-listbox';
+import { BlogCard } from '@components/blog/blog-card';
+import { SubscribeCard } from '@components/blog/subscribe-card';
+import type { GetStaticPropsResult, InferGetStaticPropsType } from 'next/types';
 import type { Variants } from 'framer-motion';
+import type { Blog, BlogWithMeta } from '@lib/types/contents';
 
 const item: Variants = {
   initial: { opacity: 0, y: 50 },
@@ -7,16 +17,85 @@ const item: Variants = {
   exit: { opacity: 0, y: 50 }
 };
 
-export default function Placeholder(): JSX.Element {
+export default function Blog({
+  tags,
+  posts
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
   return (
     <motion.main
-      className='layout min-h-screen pt-32 text-center'
+      className='grid gap-2 py-12'
       variants={item}
       initial='initial'
       animate='animate'
       exit='exit'
     >
-      Placeholder
+      <SEO
+        title='Blog | Risal Amin'
+        description='A blog by Risal Amin. My thoughts on the web, tech, and everything in between.'
+      />
+      <section className='grid gap-2'>
+        <h1 className='gradient-heading pb-1 text-5xl font-bold'>Blog</h1>
+        <p className='text-gray-600 dark:text-gray-300'>
+          My thoughts on the web, tech, and everything in between.
+        </p>
+      </section>
+      <section className='grid gap-2'>
+        <input
+          className='main-border mt-2 rounded-md px-3 py-2 outline-none transition
+                     focus:border-accent-blue dark:bg-dark-background'
+          type='text'
+          placeholder='Search blog...'
+        />
+        <section className='flex items-center gap-2'>
+          <p className='text-sm font-medium text-gray-600 dark:text-gray-300'>
+            Choose topic:
+          </p>
+          {tags.map((tag) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </section>
+        <section className='mt-4'>
+          <SortListbox />
+        </section>
+      </section>
+      <section className='mt-2 grid grid-cols-3 gap-4'>
+        {posts.map((post) => (
+          <BlogCard {...post} key={post.title} />
+        ))}
+      </section>
+      <SubscribeCard />
     </motion.main>
   );
+}
+
+type BlogProps = {
+  posts: BlogWithMeta[];
+  tags: string[];
+};
+
+export async function getStaticProps(): Promise<
+  GetStaticPropsResult<BlogProps>
+> {
+  const contentDirectory = join('src', 'pages', 'blog');
+  const contentPosts = await readdir(contentDirectory);
+
+  const posts: BlogWithMeta[] = await Promise.all(
+    contentPosts.map(async (contentPost) => {
+      const { meta } = (await import(`${contentDirectory}/${contentPost}`)) as {
+        meta: Omit<Blog, 'slug' | 'readTime'>;
+      };
+
+      const contentPath = join(contentDirectory, contentPost);
+      const readTime = await getContentReadTime(contentPath);
+
+      const slug = contentPost.replace(/\.mdx$/, '');
+      const metaFromDb = getMetaFromDb('blog', contentPost);
+
+      return { ...meta, ...metaFromDb, readTime, slug };
+    })
+  );
+
+  const tags = getTags(posts);
+
+  return { props: { posts, tags } };
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import FirstPost from '/public/assets/blogs/custom-layout-in-nextjs.webp';
+import SecondPost from '/public/assets/blogs/data-fetching-in-nextjs.webp';
+import TwitterClone from '/public/assets/projects/twitter-clone.webp';
+
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { FirstPost, SecondPost, TwitterClone } from '@lib/images';
 import { SEO } from '@components/common/seo';
 import { ReactIcon } from '@components/ui/react-icon';
-import { BlogCard } from '@components/content/blog-card';
-import { ProjectCard } from '@components/content/project-card';
+import { BlogCard } from '@components/blog/blog-card';
+import { ProjectCard } from '@components/project/project-card';
 import type { Variant } from 'framer-motion';
-import type { Blog } from '@lib/types/blog';
-import type { Project } from '@lib/types/project';
+import type { Project, BlogWithMeta } from '@lib/types/contents';
 
 type IndexVariants = Record<'hidden' | 'show' | 'exit', Variant>;
 
@@ -32,11 +34,11 @@ export default function Home(): JSX.Element {
   return (
     <main>
       <SEO
-        title='Risal Amin'
+        title='Risal Amin - Web Developer'
         description='An online portfolio and blog by Risal Amin. Showcase some of my past projects and some of my thoughts on the world of web development.'
       />
       <motion.section
-        className='layout min-h-screen pt-48'
+        className='min-h-screen pt-32'
         variants={container}
         initial='hidden'
         animate='show'
@@ -103,11 +105,11 @@ export default function Home(): JSX.Element {
           </a>
         </motion.section>
       </motion.section>
-      <section className='layout grid gap-4 py-20'>
-        <h2 className='gradient-heading text-4xl font-bold'>Featured Blogs</h2>
+      <section className='grid gap-4 py-20'>
+        <h2 className='gradient-heading text-4xl font-bold'>Featured Posts</h2>
         <section className='grid grid-cols-3 gap-4'>
-          {blogList.map((blog) => (
-            <BlogCard {...blog} key={blog.title} />
+          {blogList.map((blog, index) => (
+            <BlogCard {...blog} key={index} />
           ))}
         </section>
         <Link
@@ -117,7 +119,7 @@ export default function Home(): JSX.Element {
           See more posts
         </Link>
       </section>
-      <section className='layout grid gap-4 py-20'>
+      <section className='grid gap-4 py-20'>
         <h2 className='gradient-heading text-4xl font-bold'>
           Featured Project
         </h2>
@@ -125,8 +127,8 @@ export default function Home(): JSX.Element {
           Some projects I&apos;m proud of
         </p>
         <section className='grid grid-cols-3 gap-4'>
-          {projectList.map((project) => (
-            <ProjectCard {...project} key={project.title} />
+          {projectList.map((project, index) => (
+            <ProjectCard {...project} key={index} />
           ))}
         </section>
         <Link
@@ -140,15 +142,15 @@ export default function Home(): JSX.Element {
   );
 }
 
-const blogList: Blog[] = [
+const blogList: BlogWithMeta[] = [
   {
     slug: 'first-post',
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: FirstPost,
+    banner: FirstPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   },
@@ -157,9 +159,9 @@ const blogList: Blog[] = [
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: FirstPost,
+    banner: FirstPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   },
@@ -168,9 +170,9 @@ const blogList: Blog[] = [
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: FirstPost,
+    banner: FirstPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   },
@@ -179,9 +181,9 @@ const blogList: Blog[] = [
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: SecondPost,
+    banner: SecondPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   },
@@ -190,9 +192,9 @@ const blogList: Blog[] = [
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: SecondPost,
+    banner: SecondPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   },
@@ -201,36 +203,36 @@ const blogList: Blog[] = [
     tags: 'Next.js,Tailwind CSS,Firebase',
     views: 12_000,
     title: 'First Post',
-    image: SecondPost,
+    banner: SecondPost,
     readTime: '5 min read',
-    publishedAt: '15 December 2022',
+    publishedAt: '2022-12-15',
     description:
       'This is my first post. I will be writing about my journey as a developer and some of the things I have learned along the way. I hope you enjoy it.'
   }
 ];
 
-const projectList: Project[] = [
+const projectList: Omit<Project, 'readTime' | 'publishedAt'>[] = [
   {
     slug: 'twitter-clone',
     title: 'Twitter Clone',
-    image: TwitterClone,
-    techs: ['react', 'nextjs', 'tailwindcss', 'firebase'],
+    banner: TwitterClone,
+    techs: 'react,nextjs,tailwindcss,firebase',
     description:
       'A Twitter clone built with Next.js, Tailwind CSS, and Firebase.'
   },
   {
     slug: 'twitter-clone',
     title: 'Twitter Clone',
-    image: TwitterClone,
-    techs: ['react', 'nextjs', 'tailwindcss', 'firebase'],
+    banner: TwitterClone,
+    techs: 'react,nextjs,tailwindcss,firebase',
     description:
       'A Twitter clone built with Next.js, Tailwind CSS, and Firebase.'
   },
   {
     slug: 'twitter-clone',
     title: 'Twitter Clone',
-    image: TwitterClone,
-    techs: ['react', 'nextjs', 'tailwindcss', 'firebase'],
+    banner: TwitterClone,
+    techs: 'react,nextjs,tailwindcss,firebase',
     description:
       'A Twitter clone built with Next.js, Tailwind CSS, and Firebase.'
   }
