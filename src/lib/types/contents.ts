862a5781dc260306da25585d2b45dc6aef2254bc
ccrsxx <aminrisal@gmail.com>
2022-12-26 22:03:34 +0700

    feat: add frontmatter type

---
+++ src/lib/types/contents.ts
@@ -0,0 +1,36 @@
+import type { StaticImageData } from 'next/image';
+
+export type Blog = {
+  slug: string;
+  tags: string;
+  title: string;
+  banner: StaticImageData;
+  readTime: string;
+  description: string;
+  publishedAt: string;
+  lastUpdatedAt?: string;
+};
+
+export type Project = {
+  slug: string;
+  link?: string;
+  title: string;
+  techs: string;
+  banner: StaticImageData;
+  github?: string;
+  youtube?: string;
+  readTime: string;
+  description: string;
+  publishedAt: string;
+  lastUpdatedAt?: string;
+};
+
+export type InjectedMeta = {
+  views: number;
+  likes?: number;
+};
+
+export type ContentType = 'blog' | 'project';
+
+export type BlogWithMeta = Blog & InjectedMeta;
+export type ProjectWithMeta = Project & InjectedMeta;
