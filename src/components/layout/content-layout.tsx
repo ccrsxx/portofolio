6185db6e4a346534ec747c232a700a3f1f20fad9
ccrsxx <aminrisal@gmail.com>
2022-12-28 22:41:22 +0700

    feat: rename layout and finish most of the component

--- src/components/layout/content-layout.tsx
+++ src/components/layout/content-layout.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { HiHeart } from 'react-icons/hi2';
+import { REPOSITORY_URL } from '@lib/env';
 import { formatDate } from '@lib/format';
 import { SEO } from '@components/common/seo';
 import { BlogStats } from '@components/blog/blog-stats';
 import { TableOfContents } from '@components/content/table-of-contents';
+import { SubscribeCard } from '@components/blog/subscribe-card';
 import type { ReactElement } from 'react';
 import type { Variants } from 'framer-motion';
 import type { Blog } from '@lib/types/contents';
 import type { ContentSlugProps } from '@lib/mdx';
 
-type BlogLayoutProps = {
+type ContentLayoutProps = {
   children: ReactElement<ContentSlugProps>;
   meta: Pick<Blog, 'title' | 'publishedAt' | 'description' | 'banner' | 'tags'>;
 };
@@ -21,15 +24,17 @@ const item: Variants = {
   exit: { opacity: 0, y: 50 }
 };
 
-export function BlogLayout({
-  meta: { title, publishedAt, banner },
+export function ContentLayout({
+  meta: { title, description, publishedAt, banner },
   children
-}: BlogLayoutProps): JSX.Element {
-  const { views, likes, readTime } = children.props;
+}: ContentLayoutProps): JSX.Element {
+  const { type, slug, views, likes, readTime } = children.props;
+
+  const contentUrl = `${REPOSITORY_URL}/blob/main/src/pages/blog/${slug}.mdx`;
 
   return (
     <motion.main className='mb-12 grid gap-4' {...item}>
-      <SEO title={`${title} | Risal Amin`} />
+      <SEO title={`${title} | Risal Amin`} description={description} />
       <Image
         className='h-[448px] rounded-md object-cover'
         src={banner}
@@ -52,10 +57,33 @@ export function BlogLayout({
           {children}
         </article>
         <TableOfContents>
-          <i className='mx-auto mt-4 flex items-center gap-2 not-italic'>
-            <HiHeart className='h-10 w-10' /> {likes}
-          </i>
+          <div className='mt-4 flex items-center justify-center gap-2'>
+            <button
+              className='text-gray-400 transition-transform hover:scale-110
+                         active:scale-95 dark:text-gray-600'
+            >
+              <HiHeart className='h-12 w-12' />
+            </button>
+            <p className='text-lg text-gray-400 dark:text-gray-500'>{likes}</p>
+          </div>
         </TableOfContents>
+      </section>
+      <SubscribeCard />
+      <section
+        className='[&>a>span]:gradient-title [&>a]:animated-underline
+                   mt-4 flex justify-between font-medium'
+      >
+        <Link className='with-dots' href={`/${type}`}>
+          <span>← Back to {type}</span>
+        </Link>
+        <a
+          className='with-dots'
+          href={contentUrl}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <span>Edit this on GitHub</span>
+        </a>
       </section>
     </motion.main>
   );
