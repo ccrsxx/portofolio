baa3ac7746a9ba1374d619e2aa9759249ac1ae5c
ccrsxx <aminrisal@gmail.com>
2023-01-06 22:23:54 +0700

    feat: finish projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,11 @@
 import { motion } from 'framer-motion';
+import { getAllContents } from '@lib/mdx';
+import { SEO } from '@components/common/seo';
+import { ProjectCard } from '@components/project/project-card';
+import { Accent } from '@components/ui/accent';
+import type { GetStaticPropsResult, InferGetStaticPropsType } from 'next/types';
 import type { Variants } from 'framer-motion';
+import type { ProjectWithMeta } from '@lib/types/contents';
 
 const item: Variants = {
   initial: { opacity: 0, y: 50 },
@@ -7,16 +13,46 @@ const item: Variants = {
   exit: { opacity: 0, y: 50 }
 };
 
-export default function Placeholder(): JSX.Element {
+export default function Projects({
+  projects
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
   return (
     <motion.main
-      className='layout min-h-screen pt-32 text-center'
+      className='grid gap-2 py-12'
       variants={item}
       initial='initial'
       animate='animate'
       exit='exit'
     >
-      Placeholder
+      <SEO
+        title='Projects | Risal Amin'
+        description='A showcase of my projects on the web development.'
+      />
+      <section className='grid gap-2'>
+        <h1 className='pb-1 text-5xl font-bold'>
+          <Accent>Projects</Accent>
+        </h1>
+        <p className='text-gray-600 dark:text-gray-300'>
+          A showcase of my projects on the web development.
+        </p>
+      </section>
+      <section className='mt-4 grid grid-cols-3 gap-4'>
+        {projects.map((post) => (
+          <ProjectCard {...post} key={post.title} />
+        ))}
+      </section>
     </motion.main>
   );
+}
+
+type BlogProps = {
+  projects: ProjectWithMeta[];
+};
+
+export async function getStaticProps(): Promise<
+  GetStaticPropsResult<BlogProps>
+> {
+  const projects = await getAllContents('projects');
+
+  return { props: { projects } };
 }
