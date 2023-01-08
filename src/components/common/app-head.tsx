96f47d88080df07285bd6b929bce8d6ee62204be
ccrsxx <aminrisal@gmail.com>
2023-01-08 20:35:32 +0700

    feat: remove title in the app head

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -9,8 +9,6 @@ export function AppHead(): JSX.Element {
   return (
     <>
       <Head>
-        <title>Next.js Template</title>
-        <meta name='og:title' content='Next.js Template' />
         <link rel='icon' href='/favicon.ico' />
         <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
         <meta name='twitter:site' content='@ccrsxx' />
