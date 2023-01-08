5c813f9d9ba134d1466bc6e9f2b1bdb0d2ac2ea8
ccrsxx <aminrisal@gmail.com>
2023-01-08 20:36:31 +0700

    feat: finish about page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import { getAllContents } from '@lib/mdx';
 import { getTags } from '@lib/mdx-utils';
+import { setTransition } from '@lib/transition';
 import { SEO } from '@components/common/seo';
 import { Tag } from '@components/blog/tag';
 import { SortListbox } from '@components/blog/sort-listbox';
@@ -8,67 +9,65 @@ import { BlogCard } from '@components/blog/blog-card';
 import { SubscribeCard } from '@components/blog/subscribe-card';
 import { Accent } from '@components/ui/accent';
 import type { GetStaticPropsResult, InferGetStaticPropsType } from 'next/types';
-import type { Variants } from 'framer-motion';
 import type { BlogWithMeta } from '@lib/types/contents';
-
-const item: Variants = {
-  initial: { opacity: 0, y: 50 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 50 }
-};
 
 export default function Blog({
   tags,
   posts
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
   return (
-    <motion.main
-      className='grid gap-2 py-12'
-      variants={item}
-      initial='initial'
-      animate='animate'
-      exit='exit'
-    >
+    <main className='grid gap-2 py-12'>
       <SEO
         title='Blog | Risal Amin'
         description='A blog by Risal Amin. My thoughts on the web, tech, and everything in between.'
       />
       <section className='grid gap-2'>
-        <span className='pb-1 text-5xl font-bold'>
+        <motion.h1 className='pb-1 text-5xl font-bold' {...setTransition()}>
           <Accent>Blog</Accent>
-        </span>
-        <p className='text-gray-600 dark:text-gray-300'>
+        </motion.h1>
+        <motion.p
+          className='text-gray-600 dark:text-gray-300'
+          {...setTransition({ delayIn: 0.1 })}
+        >
           My thoughts on the web, tech, and everything in between.
-        </p>
+        </motion.p>
       </section>
       <section className='grid gap-2'>
-        <input
-          className='main-border mt-2 rounded-md px-3 py-2 outline-none transition
-                     focus:border-accent-blue dark:bg-dark-background'
-          type='text'
-          placeholder='Search blog...'
-        />
-        <section className='flex items-center gap-2'>
+        <motion.form {...setTransition({ delayIn: 0.2 })}>
+          <input
+            className='main-border mt-2 w-full rounded-md px-3 py-2 outline-none
+                       transition focus:border-accent-blue dark:bg-dark-background'
+            type='text'
+            placeholder='Search blog...'
+          />
+        </motion.form>
+        <motion.section
+          className='flex items-center gap-2'
+          {...setTransition({ delayIn: 0.3 })}
+        >
           <p className='text-sm font-medium text-gray-600 dark:text-gray-300'>
             Choose topic:
           </p>
           {tags.map((tag) => (
             <Tag key={tag}>{tag}</Tag>
           ))}
-        </section>
-        <section className='mt-4'>
+        </motion.section>
+        <motion.section className='mt-4' {...setTransition({ delayIn: 0.4 })}>
           <SortListbox />
-        </section>
+        </motion.section>
       </section>
-      <section className='mt-2 grid grid-cols-3 gap-4'>
+      <motion.section
+        className='mt-2 grid grid-cols-3 gap-4'
+        {...setTransition({ delayIn: 0.5 })}
+      >
         {posts.map((post) => (
           <BlogCard {...post} key={post.title} />
         ))}
-      </section>
-      <section className='mt-6'>
+      </motion.section>
+      <motion.section className='mt-6' {...setTransition({ delayIn: 0.6 })}>
         <SubscribeCard />
-      </section>
-    </motion.main>
+      </motion.section>
+    </main>
   );
 }
 
