b3fd5a1f8200ec2d00cce0fd741fe73c2c7fc160
ccrsxx <aminrisal@gmail.com>
2023-04-09 16:54:52 +0700

    feat: add development env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,6 @@
 export const PUBLIC_URL = process.env.NEXT_PUBLIC_URL as string;
 
-export const VALID_ORIGIN_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost' : PUBLIC_URL;
+export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 export const OWNER_BEARER_TOKEN = process.env
   .NEXT_PUBLIC_OWNER_BEARER_TOKEN as string;
