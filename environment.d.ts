82ca3965817aa224effd0f1ef4e1fc41894431b0
ccrsxx <aminrisal@gmail.com>
2023-05-16 23:50:39 +0700

    refactor: use record and extends for the custom env

--- environment.d.ts
+++ environment.d.ts
@@ -1,39 +1,43 @@
+type CustomEnvKeys =
+  // Dev URL
+  | 'NEXT_PUBLIC_URL'
+
+  // Owner Secret
+  | 'NEXT_PUBLIC_OWNER_BEARER_TOKEN'
+
+  // Email
+  | 'EMAIL_ADDRESS'
+  | 'EMAIL_PASSWORD'
+  | 'EMAIL_TARGET'
+
+  // OAuth Authentication
+  | 'NEXTAUTH_URL'
+  | 'NEXTAUTH_SECRET'
+  | 'GITHUB_ID'
+  | 'GITHUB_SECRET'
+
+  // IP Address Salt
+  | 'IP_ADDRESS_SALT'
+
+  // Firebase
+  | 'API_KEY'
+  | 'AUTH_DOMAIN'
+  | 'PROJECT_ID'
+  | 'STORAGE_BUCKET'
+  | 'MESSAGING_SENDER_ID'
+  | 'APP_ID'
+
+  // GitHub
+  | 'GITHUB_TOKEN'
+
+  // Spotify
+  | 'SPOTIFY_CLIENT_ID'
+  | 'SPOTIFY_CLIENT_SECRET'
+  | 'SPOTIFY_REFRESH_TOKEN';
+
+type CustomEnv = Record<CustomEnvKeys, string>;
+
 declare namespace NodeJS {
-  interface ProcessEnv {
-    // Dev URL
-    NEXT_PUBLIC_URL: string;
-
-    // Owner Secret
-    NEXT_PUBLIC_OWNER_BEARER_TOKEN: string;
-
-    // Email
-    EMAIL_ADDRESS: string;
-    EMAIL_PASSWORD: string;
-    EMAIL_TARGET: string;
-
-    // OAuth Authentication
-    NEXTAUTH_URL: string;
-    NEXTAUTH_SECRET: string;
-    GITHUB_ID: string;
-    GITHUB_SECRET: string;
-
-    // IP Address Salt
-    IP_ADDRESS_SALT: string;
-
-    // Firebase
-    API_KEY: string;
-    AUTH_DOMAIN: string;
-    PROJECT_ID: string;
-    STORAGE_BUCKET: string;
-    MESSAGING_SENDER_ID: string;
-    APP_ID: string;
-
-    // Github
-    GITHUB_TOKEN: string;
-
-    // Spotify
-    SPOTIFY_CLIENT_ID: string;
-    SPOTIFY_CLIENT_SECRET: string;
-    SPOTIFY_REFRESH_TOKEN: string;
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface ProcessEnv extends CustomEnv {}
 }
