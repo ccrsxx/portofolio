bc908e674282c4adcfe68a3f04eed9b13945c3f6
ccrsxx <aminrisal@gmail.com>
2023-08-03 08:38:53 +0700

    fix: breaking mdx code change by rehype-pretty-code

--- next.config.mjs
+++ next.config.mjs
@@ -19,24 +19,19 @@ const rehypePrettyCodeOptions = {
   // Keep the background or use a custom background color?
   keepBackground: false,
 
-  // Callback hooks to add custom logic to nodes when visiting
-  // them.
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }];
-    }
+  onVisitLine(element) {
+    // Add a custom class to each line
+    element.properties.className = ['line'];
   },
 
-  onVisitHighlightedLine(node) {
-    // Each line node by default has `class="line"`.
-    node.properties.className.push('highlighted');
+  onVisitHighlightedLine(element) {
+    // Add a custom class to each highlighted line
+    element.properties.className.push('highlighted');
   },
 
-  onVisitHighlightedWord(node) {
-    // Each word node has no className by default.
-    node.properties.className = ['word'];
+  onVisitHighlightedChars(element) {
+    // Add a custom class to each highlighted character
+    element.properties.className = ['word'];
   }
 };
 
