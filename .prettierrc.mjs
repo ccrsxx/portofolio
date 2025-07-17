cea54046d5f0621425422053166766322fc7c7f2
Ami <aminrisal@gmail.com>
2025-07-17 17:15:59 +0700

    chore: add prettier-plugin-tailwindcss to configuration

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 export default /** @satisfies {import('prettier').Config} */ ({
+  plugins: ['prettier-plugin-tailwindcss'],
   singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: 'none'
