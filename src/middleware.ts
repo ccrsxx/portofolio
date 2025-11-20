a9a1d488e3524d1c12a94c660e9eb98d06f5f352
Ami <ami@ccrsxx.com>
2025-11-20 17:49:54 +0700

    refactor(middleware): remove origin validation logic for cleaner middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,13 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import {
-  getOrigin,
-  getBearerToken,
-  generateNextResponse
-} from '@lib/helper-server';
-import { frontendEnv, IS_DEVELOPMENT } from '@lib/env';
+import { getBearerToken, generateNextResponse } from '@lib/helper-server';
+import { frontendEnv } from '@lib/env';
 
 export function middleware(req: NextRequest): NextResponse {
-  const origin = getOrigin(req);
-
-  const isValidOrigin = IS_DEVELOPMENT
-    ? [frontendEnv.NEXT_PUBLIC_URL, 'http://localhost:3000'].includes(
-        origin as string
-      )
-    : origin === frontendEnv.NEXT_PUBLIC_URL;
-
-  if (!isValidOrigin) return generateNextResponse(403, 'Forbidden');
-
   const bearerToken = getBearerToken(req);
 
-  if (bearerToken !== frontendEnv.NEXT_PUBLIC_OWNER_BEARER_TOKEN)
+  if (bearerToken !== frontendEnv.NEXT_PUBLIC_OWNER_BEARER_TOKEN) {
     return generateNextResponse(401, 'Unauthorized');
+  }
 
   return NextResponse.next();
 }
